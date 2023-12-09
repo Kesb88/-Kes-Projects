f0f653ac9d5189d6709eeb9cda15178e52247641
Kester Bascillo <kbascillo@myseneca.ca>
2023-12-08 19:13:58 -0500

    assignment 6 finished

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const app = express();
 const mongoose = require("mongoose");
 const session = require("express-session");
 const fileUpload = require("express-fileupload");
+const MongoDBStore = require('connect-mongodb-session')(session); 
 // Make contents folder public
 
 const dotenv = require("dotenv");
@@ -26,6 +27,14 @@ app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main');
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "/contents")));
+
+
+const store = new MongoDBStore({
+    uri: 'mongodb://localhost:27017/web322kb-2231'
+  });
+  store.on('error', function(error) {
+    console.log(error);
+  });
   
 app.use(session({
     secret: process.env.SECRET_SESSION,
