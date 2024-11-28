8ba2b9a1ea9b9b6bbf3d4967e3fc47fa6e4c6862
Kester Bascillo <kbascillo@myseneca.ca>
2024-11-28 17:15:57 -0500

    Updated version

--- controllers/loadDataController.js
+++ controllers/loadDataController.js
@@ -7,39 +7,41 @@ router.get("/rentals", (req, res) => {
   const role = req.session.role;
 
   if (!req.session.user || req.session.role !== "clerk") {
-      res.status(401).render("general/error", {
-          title: "Restricted Access",
-          message: "You are not authorized to add rentals",
-          role
-      });
-      return;
+    res.status(401).render("general/error", {
+      title: "Restricted Access",
+      message: "You are not authorized to add rentals",
+      role,
+    });
+    return;
   }
 
-  rentalModel.find()
-      .then((rentalExist) => {
-          if (rentalExist.length > 0) {
-              res.render("load-data/rentals", {
-                  title: "Load Rentals",
-                  role,
-                  message: "exist"
-              });
-          } else {
-              rentalModel.insertMany(rentals)
-                  .then(() => {
-                      res.render("load-data/rentals", {
-                          title: "Load Rentals",
-                          role,
-                          message: "added"
-                      });
-                  })
-                  .catch(err => {
-                      console.error("Error adding rentals to database", err);
-                  });
-          }
-      })
-      .catch(err => {
-          console.error("Error checking for existing rentals", err);
-      });
+  rentalModel
+    .find()
+    .then((rentalExist) => {
+      if (rentalExist.length > 0) {
+        res.render("load-data/rentals", {
+          title: "Load Rentals",
+          role,
+          message: "exist",
+        });
+      } else {
+        rentalModel
+          .insertMany(rentals)
+          .then(() => {
+            res.render("load-data/rentals", {
+              title: "Load Rentals",
+              role,
+              message: "added",
+            });
+          })
+          .catch((err) => {
+            console.error("Error adding rentals to database", err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.error("Error checking for existing rentals", err);
+    });
 });
 
 module.exports = router;
